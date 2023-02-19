09cca7109efe1a3c13a06fa26971ea7045280bd5
Nidhi Sheth <84659956+ShethNidhi06@users.noreply.github.com>
2023-02-19 11:46:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG20CS666"
+  cout << "PES1UG20CS666_Nidhi"
   return 0;
 }
